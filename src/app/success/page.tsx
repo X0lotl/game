e7c249ffaeaa5d179d672ba9f3429c8f2d9b652e
x0lotl <danil200319771@gmail.com>
2023-12-06 11:08:29 +0200

    feat: add credantials

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,6 +1,8 @@
-'use client';
-import Link from 'next/link';
-import { ROUTES } from '@/constants/routes';
+"use client";
+
+import { FC } from "react";
+import { ROUTES } from "@/constants/routes";
+import Link from "@/frontend/ui/Link/Link";
 
 const SuccessPage = () => {
   return (
@@ -8,11 +10,23 @@ const SuccessPage = () => {
       <span className='text-4xl font-bold text-green-600'>
         Ви виграли (you won if you&apos;re from England)
       </span>
+      <span className="whitespace-pre text-center">
+        Хоміченко Данило - актор, сценарист,  {'\n'}
+        Тарасенко Артем - акторка, сценарист {'\n'}
+        Фетісов Ярослав - відеомонтажер {'\n'}
+        Бачинська Катерина - операторка сценаристка, бабуся {'\n'}
+        Пелович Дмитро - розробник {'\n'}
+        Відейко Андрій - відеомонтажер {'\n'}
+      </span>
 
-      <Link className='px-24 py-2 mt-3 text-lg uppercase font-bold bg-blue-500 text-white rounded-md hover:bg-blue-700' href={ROUTES.inProgress}>
+      <Link
+        className="px-24 py-2 mt-3 text-lg uppercase font-bold bg-blue-500 text-white rounded-md hover:bg-blue-700"
+        href={ROUTES.inProgress}
+      >
         Start again
       </Link>
     </div>
-)};
+  );
+};
 
 export default SuccessPage;
