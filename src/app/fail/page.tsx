bea042e68a0ac003c9df511dc812120d7265d773
x0lotl <danil200319771@gmail.com>
2023-12-06 17:26:44 +0200

    fix deploy

--- src/app/fail/page.tsx
+++ src/app/fail/page.tsx
@@ -4,9 +4,12 @@ import { FC, useEffect, useState } from 'react';
 import { ROUTES } from '@/constants/routes';
 
 const FailPage: FC = () => {
-  const [audio] = useState(new Audio('/media/audio/fail-bg-sound.mp3'))
+  const isServer = typeof window === "undefined";
+
+  const [audio] = useState(!isServer ? new Audio('/media/audio/fail-bg-sound.mp3') : undefined)
 
   useEffect(() => {
+    if (!audio) return;
     audio.loop = false;
     audio.play()
   }, [])
@@ -16,9 +19,9 @@ const FailPage: FC = () => {
       <span className='text-4xl font-bold text-red-600'>
         Ви програли (you failed if you&apos;re from England)
       </span>
-      <Link onClick={() => audio.pause()} className='px-24 py-2 text-lg uppercase font-bold bg-blue-500 text-white rounded-md hover:bg-blue-700' href={ROUTES.inProgress}>
+      {(audio) && (<Link onClick={() => audio.pause()} className='px-24 py-2 text-lg uppercase font-bold bg-blue-500 text-white rounded-md hover:bg-blue-700' href={ROUTES.inProgress}>
         Почати спочатку
-      </Link>
+      </Link>)}
     </div>
   ) 
 };
